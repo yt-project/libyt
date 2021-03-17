ac59cf42363098a3c53c11926bcd6e9b0c1abcd8
cindytsai <6188366+cindytsai@users.noreply.github.com>
2021-03-17 13:29:09 +0800

    Push workspace.

--- src/yt_inline.cpp
+++ src/yt_inline.cpp
@@ -11,7 +11,8 @@
 // Note        :  1. Python script name is stored in "g_param_libyt.script"
 //                2. This python script must contain function of <function_name> you called.
 //
-// Parameter   :  char *function_name : the function inside python script that you want to execute.
+// Parameter   :  char *function_name : the function inside python script that you want to execute, 
+//                                      ex: def function_name():
 //
 // Return      :  YT_SUCCESS or YT_FAIL
 //-------------------------------------------------------------------------------------------------------
@@ -49,10 +50,12 @@ int yt_inline( char *function_name )
    char *CallYT = (char*) malloc( CallYT_CommandWidth*sizeof(char) );
    sprintf( CallYT, "%s.%s()", g_param_libyt.script, function_name );
 
-   if ( PyRun_SimpleString( CallYT ) == 0 )
+   if ( PyRun_SimpleString( CallYT ) == 0 ){
       log_debug( "Invoking \"%s\" ... done\n", CallYT );
-   else
+   }
+   else{
       YT_ABORT(  "Invoking \"%s\" ... failed\n", CallYT );
+   }
 
    free( CallYT );
 
@@ -61,3 +64,45 @@ int yt_inline( char *function_name )
    return YT_SUCCESS;
 
 } // FUNCTION : yt_inline
+
+//-------------------------------------------------------------------------------------------------------
+// Function    :  yt_inline
+// Description :  Execute the YT inline analysis script
+//
+// Note        :  1. Python script name is stored in "g_param_libyt.script"
+//                2. This python script must contain function of <function_name> you called.
+//
+// Parameter   :  char *function_name  : the function inside python script that you want to execute, 
+//                                       ex: def function_name():
+//                int     argc         : input arguments length
+//                char  **argv         : input parameter
+//
+// Return      :  YT_SUCCESS or YT_FAIL
+//-------------------------------------------------------------------------------------------------------
+int yt_inline(char *function_name, int argc, char **argv){
+// check if libyt has been initialized
+   if ( !g_param_libyt.libyt_initialized ){
+      YT_ABORT( "Please invoke yt_init() before calling %s()!\n", __FUNCTION__ );
+   }
+
+// check if YT parameters have been set
+   if ( !g_param_libyt.param_yt_set ){
+      YT_ABORT( "Please invoke yt_set_parameter() before calling %s()!\n", __FUNCTION__ );
+   }
+
+// check if user has call yt_get_gridsPtr(), so that libyt knows the local grids array ptr.
+   if ( !g_param_libyt.get_gridsPtr ){
+      YT_ABORT( "Please invoke yt_get_gridsPtr() before calling %s()!\n", __FUNCTION__ );
+   }
+
+// check if user has call yt_commit_grids(), so that grids are appended to YT.
+   if ( !g_param_libyt.commit_grids ){
+      YT_ABORT( "Please invoke yt_commit_grids() before calling %s()!\n", __FUNCTION__ );
+   }
+
+// Not sure if we need this MPI_Barrier
+   MPI_Barrier(MPI_COMM_WORLD);
+
+
+   return YT_SUCCESS;
+}
