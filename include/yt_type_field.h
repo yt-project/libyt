f2d66c70401db24b60dc8cbf8c686ab0832e0a69
cindytsai <6188366+cindytsai@users.noreply.github.com>
2021-04-09 18:04:14 +0800

    Update yt_field.h.

--- include/yt_type_field.h
+++ include/yt_type_field.h
@@ -9,6 +9,9 @@
 /
 ********************************************************************************/
 
+// include relevant headers/prototypes
+void log_debug( const char *Format, ... );
+
 //-------------------------------------------------------------------------------------------------------
 // Structure   :  yt_field
 // Description :  Data structure to store a field's label and its definition of data representation.
@@ -19,10 +22,55 @@
 //
 // Method      :  yt_field  : Constructor
 //               ~yt_field  : Destructor
-//                show      : Show the value.
-//                validate  : Check if all data members have been set properly by users
+//                show      : Show the value
 //-------------------------------------------------------------------------------------------------------
 struct yt_field
 {
-	
+// data members
+// ======================================================================================================
+	char *field_name;
+	char *field_type;
+
+//=======================================================================================================
+// Method      : yt_field
+// Description : Constructor of the structure "yt_field"
+// 
+// Note        : 1. Initialize field_type as "cell-centered"
+// 
+// Parameter   : None
+// ======================================================================================================
+	yt_field()
+	{
+		field_name = "NOT SET";
+		field_type = "cell-centered";
+	} // METHOD : yt_field
+
+//=======================================================================================================
+// Method      : ~yt_field
+// Description : Destructor of the structure "yt_field"
+// 
+// Note        : 1. Not used currently
+// 
+// Parameter   : None
+//=======================================================================================================
+	~yt_field()
+	{
+
+	} // METHOD : ~yt_field
+
+//=======================================================================================================
+// Method      : show
+// Description : Print out all data members if the verbose level >= YT_VERBOSE_DEBUG
+// 
+// Note        : 1. Will be used in yt_type_param_yt.h (struct yt_param_yt show())
+// 
+// Parameter   : None
+// 
+// Return      : YT_SUCCESS
+//=======================================================================================================
+	int show() const
+	{
+		log_debug("(%s, %s)", field_name, field_type);
+	}
+
 };
