f299e3f27f6082c690edea6d4a9369eec862c155
cindytsai <6188366+cindytsai@users.noreply.github.com>
2021-04-09 17:31:18 +0800

    Create header file yt_type_field.h.

---
+++ include/yt_type_field.h
@@ -0,0 +1,28 @@
+#ifndef __YT_TYPE_FIELD_H__
+#define __YT_TYPE_FIELD_H__
+
+/*******************************************************************************
+/
+/  yt_field structure
+/
+/  ==> included by yt_type.h
+/
+********************************************************************************/
+
+//-------------------------------------------------------------------------------------------------------
+// Structure   :  yt_field
+// Description :  Data structure to store a field's label and its definition of data representation.
+// 
+// Notes       :  1. The data representation type will be initialize as cell-centered.
+//
+// Data Member :  
+//
+// Method      :  yt_field  : Constructor
+//               ~yt_field  : Destructor
+//                show      : Show the value.
+//                validate  : Check if all data members have been set properly by users
+//-------------------------------------------------------------------------------------------------------
+struct yt_field
+{
+	
+};
