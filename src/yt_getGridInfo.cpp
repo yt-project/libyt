147ffa9be1941753c97284e2fe376e0c187daa7b
cindytsai <6188366+cindytsai@users.noreply.github.com>
2021-06-29 15:06:56 +0800

    Update check point for yt_getGridInfo.cpp

--- src/yt_getGridInfo.cpp
+++ src/yt_getGridInfo.cpp
@@ -2,11 +2,51 @@
 #include "libyt.h"
 
 //-------------------------------------------------------------------------------------------------------
+// Function    :  check_procedure
+// Description :  Check if libyt is properly set.
+//
+// Note        :  1. This function will only be use in yt_getGridInfo_*().
+//
+// Parameter   :  const char *callFunc : function that calls check_procedure.
+//
+// Return      :  YT_SUCCESS or YT_FAIL
+//-------------------------------------------------------------------------------------------------------
+int check_procedure( const char *callFunc ){
+
+	// check if libyt has been initialized
+   	if ( !g_param_libyt.libyt_initialized ){
+    	YT_ABORT( "Please follow the libyt procedure, forgot to invoke yt_init() before calling %s()!\n", callFunc );
+   	}
+
+	// check if YT parameters have been set
+   	if ( !g_param_libyt.param_yt_set ){
+    	YT_ABORT( "Please follow the libyt procedure, forgot to invoke yt_set_parameter() before calling %s()!\n", callFunc );
+   	}
+
+	// check if user has call yt_get_fieldsPtr()
+   	if ( !g_param_libyt.get_fieldsPtr ){
+    	YT_ABORT( "Please follow the libyt procedure, forgot to invode yt_get_fieldsPtr() before calling %s()!\n", callFunc );
+   	}
+
+	// check if user has call yt_get_gridsPtr(), so that libyt knows the local grids array ptr.
+   	if ( !g_param_libyt.get_gridsPtr ){
+      	YT_ABORT( "Please follow the libyt procedure, forgot to invoke yt_get_gridsPtr() before calling %s()!\n", callFunc );
+   	}
+
+	// check if user has call yt_commit_grids(), so that grids are appended to YT.
+   	if ( !g_param_libyt.commit_grids ){
+      	YT_ABORT( "Please follow the libyt procedure, forgot to invoke yt_commit_grids() before calling %s()!\n", callFunc );
+   	}
+
+	return YT_SUCCESS;
+}
+//-------------------------------------------------------------------------------------------------------
 // Function    :  yt_getGridInfo_Dimensions
 // Description :  Get dimension of the grid with grid id = gid.
 //
 // Note        :  1. This function will be called inside user's field derived_func.
 //                2. Return YT_FAIL if cannot find grid id = gid.
+//                3. grid_dimensions is defined in [x][y][z] <-> [0][1][2] coordinate.
 //
 // Parameter   :  const long  gid               : Target grid id
 //                int         (*dimensions)[3]  : Write result to this pointer.
@@ -17,6 +57,11 @@
 // Return      :  YT_SUCCESS or YT_FAIL
 //-------------------------------------------------------------------------------------------------------
 int yt_getGridInfo_Dimensions( const long gid, int (*dimensions)[3] ){
+
+	if ( check_procedure( __FUNCTION__ ) != YT_SUCCESS ){
+		log_error( "Please follow the libyt procedure, terminating the process.\n" );
+		abort();
+	}
 
 	bool have_Grid = false;
 
@@ -61,6 +106,11 @@ int yt_getGridInfo_Dimensions( const long gid, int (*dimensions)[3] ){
 // Return      :  YT_SUCCESS or YT_FAIL
 //-------------------------------------------------------------------------------------------------------
 int yt_getGridInfo_FieldData( const long gid, const char *field_name, void **field_data){
+
+	if ( check_procedure( __FUNCTION__ ) != YT_SUCCESS ){
+		log_error( "Please follow the libyt procedure, terminating the process.\n" );
+		abort();
+	}
 
 	bool have_Grid  = false;
 	bool have_Field = false;
