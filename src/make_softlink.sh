5e3c75cbcd4d68df0a1db1f994e3ddd75bfabb4c
cindytsai <turquoisea.tsai@gmail.com>
2021-07-04 13:20:22 +0800

    Update comments in make_softlink.sh

--- src/make_softlink.sh
+++ src/make_softlink.sh
@@ -1,3 +1,3 @@
-# If Makefile didn't generate soname and linker name, run this file.
+# If you delete libyt soname and linker name, run this file.
 ln -s libyt.so.1.0.0 libyt.so.1
 ln -s libyt.so.1     libyt.so
