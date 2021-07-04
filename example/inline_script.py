c29d6fe0e01713ba712da0ad071106882ae0a09c
cindytsai <turquoisea.tsai@gmail.com>
2021-07-04 13:09:37 +0800

    Update example inline_script.py description.

--- example/inline_script.py
+++ example/inline_script.py
@@ -1,11 +1,17 @@
 import yt
 
+# Must include this line, if you are running in parallel.
 yt.enable_parallelism()
 
 def yt_inline_ProjectionPlot( fields ):
+    
+    # Load the data, just like using yt.load()
     ds = yt.frontends.libyt.libytDataset()
+    
+    # Do yt operation
     prjz = yt.ProjectionPlot(ds, 'z', fields)
 
+    # Include this line, otherwise yt will save one copy in each rank.
     if yt.is_root():
         prjz.save()
     
