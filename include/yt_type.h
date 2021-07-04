bd17199fae03a661993c86edc07e3e9a004bc89b
cindytsai <6188366+cindytsai@users.noreply.github.com>
2021-07-04 14:43:02 +0800

    Make yt_hierarchy inaccessible for user.

--- include/yt_type.h
+++ include/yt_type.h
@@ -26,31 +26,5 @@ enum yt_ftype   { YT_FTYPE_UNKNOWN=0, YT_FLOAT=1, YT_DOUBLE=2 };
 #include "yt_type_grid.h"
 #include "yt_type_field.h"
 
-//-------------------------------------------------------------------------------------------------------
-// Structure   :  yt_hierarchy
-// Description :  Data structure for pass hierarchy of the grid in MPI process, it is meant to be temperary.
-//
-// Data Member :  dimensions     : Number of cells along each direction
-//                left_edge      : Grid left  edge in code units
-//                right_edge     : Grid right edge in code units
-//                particle_count : Nunber of particles in this grid
-//                level          : AMR level (0 for the root level)
-//                proc_num       : An array of MPI rank that the grid belongs
-//                id             : Grid ID (0-indexed ==> must be in the range 0 <= id < total number of grids)
-//                parent_id      : Parent grid ID (0-indexed, -1 for grids on the root level)
-//                proc_num       : Process number, grid belong to which MPI rank
-//-------------------------------------------------------------------------------------------------------
-struct yt_hierarchy{
-      double left_edge[3];
-      double right_edge[3];
-
-      long   particle_count;
-      long   id;
-      long   parent_id;
-
-      int    dimensions[3];
-      int    level;
-      int    proc_num;
-};
 
 #endif // #ifndef __YT_TYPE_H__
