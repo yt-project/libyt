a9be19b4774569df4ab2812b325cadc0d1253909
cindytsai <6188366+cindytsai@users.noreply.github.com>
2021-09-07 16:36:32 +0800

    Safety check for get_npy_dtype func.

--- src/get_npy_dtype.cpp
+++ src/get_npy_dtype.cpp
@@ -22,7 +22,7 @@
 // Return      :  YT_SUCCESS or YT_FAIL
 //-------------------------------------------------------------------------------------------------------
 int get_npy_dtype( yt_dtype data_type, int *npy_dtype ){
-
+	
 	if ( data_type == YT_FLOAT ){
 		*npy_dtype = NPY_FLOAT;
 	}
@@ -36,6 +36,23 @@ int get_npy_dtype( yt_dtype data_type, int *npy_dtype ){
 		*npy_dtype = NPY_LONG;
 	}
 	else{
+		// Safety check that data_type is one of yt_dtype, 
+		// so that if we cannot match a NumPy Enum Type, then it must be user forgot to implement here.
+		bool valid = false;
+		for ( int yt_dtypeInt = YT_FLOAT; yt_dtypeInt < YT_DTYPE_UNKNOWN; yt_dtypeInt++ ){
+			yt_dtype dtype = static_cast<yt_dtype>(yt_dtypeInt);
+			if ( data_type == dtype ){
+				valid = true;
+				break;
+			}
+		}
+		if ( valid == true ){
+			log_error("You should also match your new yt_dtype to NumPy enumerate type in get_npy_dtype function.\n");
+		}
+		else{
+			log_error("If you want to implement your new yt_dtype, you should modify both yt_dtype Enum and get_npy_dtype function.\n");
+		}
+		
 		*npy_dtype = -1;
 		return YT_FAIL;
 	}
