860442534f1544285d454189fad0f40be6ed4547
cindytsai <6188366+cindytsai@users.noreply.github.com>
2021-09-22 14:44:52 +0800

    Update workspace,  test on SlicePlot.

--- example/inline_script.py
+++ example/inline_script.py
@@ -4,22 +4,23 @@ import yt
 yt.enable_parallelism()
 
 def yt_inline_ProjectionPlot( fields ):
+    pass
+    # # Load the data, just like using yt.load()
+    # ds = yt.frontends.libyt.libytDataset()
     
-    # Load the data, just like using yt.load()
-    ds = yt.frontends.libyt.libytDataset()
-    
-    # Do yt operation
-    prjz = yt.ProjectionPlot(ds, 'z', fields)
+    # # Do yt operation
+    # prjz = yt.ProjectionPlot(ds, 'z', fields)
 
-    # Include this line, otherwise yt will save one copy in each rank.
-    if yt.is_root():
-        prjz.save()
+    # # Include this line, otherwise yt will save one copy in each rank.
+    # if yt.is_root():
+    #     prjz.save()
 
 def yt_inline_ProfilePlot():
-    ds = yt.frontends.libyt.libytDataset()
-    profile = yt.ProfilePlot(ds, "x", ["density"])
-    if yt.is_root():
-        profile.save()
+    pass
+    # ds = yt.frontends.libyt.libytDataset()
+    # profile = yt.ProfilePlot(ds, "x", ["density"])
+    # if yt.is_root():
+    #     profile.save()
     
 def yt_inline_ParticlePlot():
     pass
@@ -37,5 +38,7 @@ def yt_inline_ParticlePlot():
     # par.save()
 
 def test_user_parameter():
-    import libyt
-    print("user_int = ", libyt.param_user['user_int'])
+    ds = yt.frontends.libyt.libytDataset()
+    slc = yt.SlicePlot(ds, "z", ("gas", "density"))
+    if yt.is_root():
+        slc.save()
