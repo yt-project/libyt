6f519e77c3e367b962e183bb614ded08b062a21b
cindytsai <6188366+cindytsai@users.noreply.github.com>
2022-03-13 01:03:34 +0800

    Timer class.

---
+++ include/Timer.h
@@ -0,0 +1,27 @@
+#ifndef __TIMER_H__
+#define __TIMER_H__
+
+#include <vector>
+#include <string>
+
+class Timer
+{
+private:
+    char  m_FileName[50];
+    long  m_Step;
+
+    std::vector<double> m_RecordTime;
+    std::vector<bool>   m_CheckRecordTime;
+    std::vector<std::string> m_Column;
+
+public:
+    Timer(char *filename);
+    ~Timer();
+
+    void print_header();
+    void record_time(char *Column, int tag);
+    void print_all_time();
+};
+
+
+#endif //__TIMER_H__
