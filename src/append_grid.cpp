2f883e9c3c2a170d2288296075eb7db4b6ceb70c
cindytsai <6188366+cindytsai@users.noreply.github.com>
2022-11-02 11:34:03 -0500

    Do not create key-value pair if no data to wrap.

--- src/append_grid.cpp
+++ src/append_grid.cpp
@@ -42,103 +42,91 @@ int append_grid( yt_grid *grid ){
     FILL_ARRAY( "proc_num",            &grid->proc_num,            1, npy_int    )
     log_debug( "Inserting grid [%ld] info to libyt.hierarchy ... done\n", grid->id );
 
-// export grid data to libyt.grid_data as "libyt.grid_data[grid_id][field_list.field_name][field_data.data_ptr]"
+    // return if no field data, no need to do anything.
+    if ( grid->field_data == NULL ) return YT_SUCCESS;
+
+    // export grid data to libyt.grid_data as "libyt.grid_data[grid_id][field_list.field_name][field_data.data_ptr]"
+    // if there is data to append.
     PyObject *py_grid_id, *py_field_labels, *py_field_data;
 
-// allocate [grid_id][field_list.field_name]
-    py_grid_id      = PyLong_FromLong( grid->id );
+    // append data to dict only if data is not NULL.
+    py_grid_id = PyLong_FromLong( grid->id );
     py_field_labels = PyDict_New();
+    for (int v=0; v<g_param_yt.num_fields; v++) {
+        if ( (grid->field_data)[v].data_ptr == NULL ) continue;
 
-    PyDict_SetItem( g_py_grid_data, py_grid_id, py_field_labels );
-
-// fill [grid_id][field_list.field_name][field_data.data_ptr]
-    for (int v=0; v<g_param_yt.num_fields; v++)
-    {
-//    If grid->field_data == NULL, append Py_None to dictionary
-        if ( grid->field_data == NULL ) {
-            // add Py_None to dict "libyt.grid_data[grid_id][field_list.field_name]"
-            PyDict_SetItemString( py_field_labels, g_param_yt.field_list[v].field_name, Py_None );
+        // check if dictionary exists, if no add new dict under key gid
+        if ( PyDict_Contains( g_py_grid_data, py_grid_id ) != 1 ) {
+            PyDict_SetItem( g_py_grid_data, py_grid_id, py_field_labels );
         }
 
-//    Else grid->field_data != NULL, append the data.
-//    Or else append Py_None.
-        else {
-            if ( (grid->field_data)[v].data_ptr == NULL ){
-                // add Py_None to dict "libyt.grid_data[grid_id][field_list.field_name]"
-                PyDict_SetItemString( py_field_labels, g_param_yt.field_list[v].field_name, Py_None );
-
-                log_debug( "Inserting [ None ] to grid [%ld] field data [%s] to libyt.grid_data, since data_ptr == NULL\n",
-                           grid->id, g_param_yt.field_list[v].field_name );
-            }
-            else {
-                // Grab NumPy Enumerate Type in order: (1)data_dtype (2)field_dtype
-                int grid_dtype;
-                if ( get_npy_dtype((grid->field_data)[v].data_dtype, &grid_dtype) == YT_SUCCESS ){
-                    log_debug("Grid ID [ %ld ], field data [ %s ], grab NumPy enumerate type from data_dtype.\n",
-                              grid->id, g_param_yt.field_list[v].field_name);
-                }
-                else if ( get_npy_dtype(g_param_yt.field_list[v].field_dtype, &grid_dtype) == YT_SUCCESS ){
-                    (grid->field_data)[v].data_dtype = g_param_yt.field_list[v].field_dtype;
-                    log_debug("Grid ID [ %ld ], field data [ %s ], grab NumPy enumerate type from field_dtype.\n",
-                              grid->id, g_param_yt.field_list[v].field_name);
-                }
-                else{
-                    YT_ABORT("Grid ID [ %ld ], field data [ %s ], cannot get the NumPy enumerate type properly.\n",
-                             grid->id, g_param_yt.field_list[v].field_name);
-                }
-
-                // Get the dimension of the input array
-                // Only "cell-centered" will be set to grid_dimensions + ghost cell, else should be set in data_dimensions.
-                if ( strcmp(g_param_yt.field_list[v].field_define_type, "cell-centered") == 0 ){
-                    // Get grid_dimensions and consider swap_axes or not, since grid_dimensions is defined as [x][y][z].
-                    if ( g_param_yt.field_list[v].swap_axes == true ){
-                        (grid->field_data)[v].data_dimensions[0] = (grid->grid_dimensions)[2];
-                        (grid->field_data)[v].data_dimensions[1] = (grid->grid_dimensions)[1];
-                        (grid->field_data)[v].data_dimensions[2] = (grid->grid_dimensions)[0];
-                    }
-                    else{
-                        (grid->field_data)[v].data_dimensions[0] = (grid->grid_dimensions)[0];
-                        (grid->field_data)[v].data_dimensions[1] = (grid->grid_dimensions)[1];
-                        (grid->field_data)[v].data_dimensions[2] = (grid->grid_dimensions)[2];
-                    }
-                    // Plus the ghost cell to get the actual array dimensions.
-                    for(int d = 0; d < 6; d++) {
-                        (grid->field_data)[v].data_dimensions[ d / 2 ] += g_param_yt.field_list[v].field_ghost_cell[d];
-                    }
-                }
-                // See if all data_dimensions > 0, abort if not.
-                for (int d = 0; d < 3; d++){
-                    if ( (grid->field_data)[v].data_dimensions[d] <= 0 ){
-                        YT_ABORT("Grid ID [ %ld ], field data [ %s ], data_dimensions[%d] = %d <= 0.\n",
-                                 grid->id, g_param_yt.field_list[v].field_name, d, (grid->field_data)[v].data_dimensions[d]);
-                    }
-                }
-
-                npy_intp grid_dims[3] = { (grid->field_data)[v].data_dimensions[0],
-                                          (grid->field_data)[v].data_dimensions[1],
-                                          (grid->field_data)[v].data_dimensions[2]};
-
-                // PyArray_SimpleNewFromData simply creates an array wrapper and does not allocate and own the array
-                py_field_data = PyArray_SimpleNewFromData( 3, grid_dims, grid_dtype, (grid->field_data)[v].data_ptr );
-
-                // Mark this memory (NumPy array) read-only
-                PyArray_CLEARFLAGS( (PyArrayObject*) py_field_data, NPY_ARRAY_WRITEABLE);
-
-                // add the field data to dict "libyt.grid_data[grid_id][field_list.field_name]"
-                PyDict_SetItemString( py_field_labels, g_param_yt.field_list[v].field_name, py_field_data );
-
-                // call decref since PyDict_SetItemString() returns a new reference
-                Py_DECREF( py_field_data );
-
-                log_debug( "Inserting grid [%ld] field data [%s] to libyt.grid_data ... done\n",
-                           grid->id, g_param_yt.field_list[v].field_name );
-            }
-
+        // insert data under py_field_labels dict
+        // (1) Grab NumPy Enumerate Type in order: (1)data_dtype (2)field_dtype
+        int grid_dtype;
+        if ( get_npy_dtype((grid->field_data)[v].data_dtype, &grid_dtype) == YT_SUCCESS ){
+            log_debug("Grid ID [ %ld ], field data [ %s ], grab NumPy enumerate type from data_dtype.\n",
+                      grid->id, g_param_yt.field_list[v].field_name);
         }
+        else if ( get_npy_dtype(g_param_yt.field_list[v].field_dtype, &grid_dtype) == YT_SUCCESS ){
+            (grid->field_data)[v].data_dtype = g_param_yt.field_list[v].field_dtype;
+            log_debug("Grid ID [ %ld ], field data [ %s ], grab NumPy enumerate type from field_dtype.\n",
+                      grid->id, g_param_yt.field_list[v].field_name);
+        }
+        else{
+            YT_ABORT("Grid ID [ %ld ], field data [ %s ], cannot get the NumPy enumerate type properly.\n",
+                     grid->id, g_param_yt.field_list[v].field_name);
+        }
+
+        // (2) Get the dimension of the input array
+        // Only "cell-centered" will be set to grid_dimensions + ghost cell, else should be set in data_dimensions.
+        if ( strcmp(g_param_yt.field_list[v].field_define_type, "cell-centered") == 0 ){
+            // Get grid_dimensions and consider swap_axes or not, since grid_dimensions is defined as [x][y][z].
+            if ( g_param_yt.field_list[v].swap_axes == true ){
+                (grid->field_data)[v].data_dimensions[0] = (grid->grid_dimensions)[2];
+                (grid->field_data)[v].data_dimensions[1] = (grid->grid_dimensions)[1];
+                (grid->field_data)[v].data_dimensions[2] = (grid->grid_dimensions)[0];
+            }
+            else{
+                (grid->field_data)[v].data_dimensions[0] = (grid->grid_dimensions)[0];
+                (grid->field_data)[v].data_dimensions[1] = (grid->grid_dimensions)[1];
+                (grid->field_data)[v].data_dimensions[2] = (grid->grid_dimensions)[2];
+            }
+            // Plus the ghost cell to get the actual array dimensions.
+            for(int d = 0; d < 6; d++) {
+                (grid->field_data)[v].data_dimensions[ d / 2 ] += g_param_yt.field_list[v].field_ghost_cell[d];
+            }
+        }
+        // See if all data_dimensions > 0, abort if not.
+        for (int d = 0; d < 3; d++){
+            if ( (grid->field_data)[v].data_dimensions[d] <= 0 ){
+                YT_ABORT("Grid ID [ %ld ], field data [ %s ], data_dimensions[%d] = %d <= 0.\n",
+                         grid->id, g_param_yt.field_list[v].field_name, d, (grid->field_data)[v].data_dimensions[d]);
+            }
+        }
+
+        npy_intp grid_dims[3] = { (grid->field_data)[v].data_dimensions[0],
+                                  (grid->field_data)[v].data_dimensions[1],
+                                  (grid->field_data)[v].data_dimensions[2]};
+
+        // (3) Insert data to dict
+        // PyArray_SimpleNewFromData simply creates an array wrapper and does not allocate and own the array
+        py_field_data = PyArray_SimpleNewFromData( 3, grid_dims, grid_dtype, (grid->field_data)[v].data_ptr );
+
+        // Mark this memory (NumPy array) read-only
+        PyArray_CLEARFLAGS( (PyArrayObject*) py_field_data, NPY_ARRAY_WRITEABLE);
+
+        // add the field data to dict "libyt.grid_data[grid_id][field_list.field_name]"
+        PyDict_SetItemString( py_field_labels, g_param_yt.field_list[v].field_name, py_field_data );
+
+        // call decref since PyDict_SetItemString() returns a new reference
+        Py_DECREF( py_field_data );
+
+        log_debug( "Inserting grid [%ld] field data [%s] to libyt.grid_data ... done\n",
+                   grid->id, g_param_yt.field_list[v].field_name );
 
     }
 
-// call decref since both PyLong_FromLong() and PyDict_New() return a new reference
+    // call decref since both PyLong_FromLong() and PyDict_New() return a new reference
     Py_DECREF( py_grid_id );
     Py_DECREF( py_field_labels );
 
