2d1a5a8d7732b2453bdc09a610dd83223bb09d7d
cindytsai <6188366+cindytsai@users.noreply.github.com>
2022-12-20 11:40:52 -0600

    Wrap inline function in try/except when calling.

--- src/yt_inline.cpp
+++ src/yt_inline.cpp
@@ -11,8 +11,7 @@
 //                2. This python script must contain function of <function_name> you called.
 //                3. Must give argc (argument count), even if there are no arguments.
 //                4. Under INTERACTIVE_MODE, function will be wrapped inside try/except. If there is error
-//                   it will store under libyt.interactive_mode["func_status"]["func_name"]. Otherwise,
-//                   this variable will be None.
+//                   it will store under libyt.interactive_mode["func_err_msg"]["func_name"].
 //
 // Parameter   :  char *function_name : function name in python script 
 //                int  argc           : input arguments count
@@ -26,12 +25,12 @@ int yt_inline_argument(char *function_name, int argc, ...) {
     g_timer->record_time(function_name, 0);
 #endif
 
-// check if libyt has been initialized
+    // check if libyt has been initialized
     if (!g_param_libyt.libyt_initialized) {
         YT_ABORT("Please invoke yt_init() before calling %s()!\n", __FUNCTION__);
     }
 
-// Not sure if we need this MPI_Barrier
+    // every rank run at the same time.
     MPI_Barrier(MPI_COMM_WORLD);
 
     log_info("Performing YT inline analysis ...\n");
@@ -43,9 +42,7 @@ int yt_inline_argument(char *function_name, int argc, ...) {
     // Count inline function width = .<function_name>() + '\0'
     int InlineFunctionWidth = strlen(function_name) + 4;
     for (int i = 0; i < argc; i++) {
-
         if (i != 0) InlineFunctionWidth++; // comma "," in called function
-
         InlineFunctionWidth = InlineFunctionWidth + strlen(va_arg(Args_len, char * ));
     }
 
@@ -57,9 +54,7 @@ int yt_inline_argument(char *function_name, int argc, ...) {
     strcat(CallYT, function_name);
     strcat(CallYT, "(");
     for (int i = 0; i < argc; i++) {
-
         if (i != 0) strcat(CallYT, ",");
-
         strcat(CallYT, va_arg(Args, char * ));
     }
     strcat(CallYT, ")");
@@ -67,14 +62,34 @@ int yt_inline_argument(char *function_name, int argc, ...) {
     va_end(Args_len);
     va_end(Args);
 
-    if (PyRun_SimpleString(CallYT) == 0) {
-        log_debug("Invoking \"%s\" ... done\n", CallYT);
-    } else {
-        YT_ABORT("Invoking \"%s\" ... failed\n", CallYT);
-    }
+#ifdef INTERACTIVE_MODE
+    // put command in try ... except
+    // 101 -> try: ... except: ...
+    //   4 -> newline
+    // scipt_name.func_name(...) -> strlen(script_name) + InlineFunctionWidth(contain '\0')
+    // func_name -> strlen(function_name)
+    char *CallYT_TryExcept = (char *) malloc((101 + 4 + strlen(g_param_libyt.script) + strlen(function_name) + InlineFunctionWidth)
+                                             * sizeof(char));
+    sprintf(CallYT_TryExcept, "try:\n"
+                              "    %s\n"
+                              "except Exception as e:\n"
+                              "    libyt.interactive_mode[\"func_err_msg\"][\"%s\"] = traceback.format_exc()\n",
+                              CallYT, function_name);
+#endif // #ifdef INTERACTIVE_MODE
+
+#ifdef INTERACTIVE_MODE
+    if (PyRun_SimpleString(CallYT_TryExcept) == 0) log_debug("Invoking \"%s\" in interactive mode ... done\n", CallYT);
+#else
+    if (PyRun_SimpleString(CallYT) == 0)           log_debug("Invoking \"%s\" ... done\n", CallYT);
+#endif // #ifdef INTERACTIVE_MODE
+    else YT_ABORT("Invoking \"%s\" ... failed\n", CallYT);
 
     log_info("Performing YT inline analysis <%s> ... done.\n", CallYT);
+
     free(CallYT);
+#ifdef INTERACTIVE_MODE
+    free(CallYT_TryExcept);
+#endif
 
 #ifdef SUPPORT_TIMER
     g_timer->record_time(function_name, 1);
@@ -91,8 +106,7 @@ int yt_inline_argument(char *function_name, int argc, ...) {
 //                2. This python script must contain function of <function_name> you called.
 //                3. This python function must not contain input arguments.
 //                4. Under INTERACTIVE_MODE, function will be wrapped inside try/except. If there is error
-//                   it will store under libyt.interactive_mode["func_status"]["func_name"]. Otherwise,
-//                   this variable will be None.
+//                   it will store under libyt.interactive_mode["func_err_msg"]["func_name"].
 //
 // Parameter   :  char *function_name : function name in python script
 //
@@ -103,12 +117,12 @@ int yt_inline(char *function_name) {
     g_timer->record_time(function_name, 0);
 #endif
 
-// check if libyt has been initialized
+    // check if libyt has been initialized
     if (!g_param_libyt.libyt_initialized) {
         YT_ABORT("Please invoke yt_init() before calling %s()!\n", __FUNCTION__);
     }
 
-// Not sure if we need this MPI_Barrier
+    // every rank run at the same time
     MPI_Barrier(MPI_COMM_WORLD);
 
     log_info("Performing YT inline analysis ...\n");
@@ -118,14 +132,34 @@ int yt_inline(char *function_name) {
     char *CallYT = (char *) malloc(CallYT_CommandWidth * sizeof(char));
     sprintf(CallYT, "%s.%s()", g_param_libyt.script, function_name);
 
-    if (PyRun_SimpleString(CallYT) == 0) {
-        log_debug("Invoking \"%s\" ... done\n", CallYT);
-    } else {
-        YT_ABORT("Invoking \"%s\" ... failed\n", CallYT);
-    }
+#ifdef INTERACTIVE_MODE
+    // put command in try ... except
+    // 101 -> try: ... except: ...
+    //   4 -> newline
+    // scipt_name.func_name() -> strlen(script_name) + InlineFunctionWidth(contain '\0')
+    // func_name -> strlen(function_name)
+    char *CallYT_TryExcept = (char *) malloc((101 + 4 + strlen(g_param_libyt.script) + strlen(function_name) + InlineFunctionWidth)
+                                             * sizeof(char));
+    sprintf(CallYT_TryExcept, "try:\n"
+                              "    %s\n"
+                              "except Exception as e:\n"
+                              "    libyt.interactive_mode[\"func_err_msg\"][\"%s\"] = traceback.format_exc()\n",
+                              CallYT, function_name);
+#endif // #ifdef INTERACTIVE_MODE
+
+#ifdef INTERACTIVE_MODE
+    if (PyRun_SimpleString(CallYT_TryExcept) == 0) log_debug("Invoking \"%s\" in interactive mode ... done\n", CallYT);
+#else
+    if (PyRun_SimpleString(CallYT) == 0)           log_debug("Invoking \"%s\" ... done\n", CallYT);
+#endif // #ifdef INTERACTIVE_MODE
+    else YT_ABORT("Invoking \"%s\" ... failed\n", CallYT);
 
     log_info("Performing YT inline analysis <%s> ... done.\n", CallYT);
+
     free(CallYT);
+#ifdef INTERACTIVE_MODE
+    free(CallYT_TryExcept);
+#endif
 
 #ifdef SUPPORT_TIMER
     g_timer->record_time(function_name, 1);
