379101d9a2bafe54885bb2ac873a7674ff76b0fd
cindytsai <6188366+cindytsai@users.noreply.github.com>
2022-12-22 17:13:15 -0600

    Update func_status class.

--- include/func_status.h
+++ include/func_status.h
@@ -2,7 +2,6 @@
 #define __FUNC_STATUS_H__
 
 #include "yt_macro.h"
-#include <string.h>
 #include <string>
 
 class func_status
@@ -11,7 +10,7 @@ private:
     char       *m_FuncName;
     std::string m_FuncBody;
     bool        m_Run;
-    int         m_Status;
+    short       m_Status;
 
 public:
     func_status(char *func_name);
@@ -21,8 +20,8 @@ public:
     int clear_func_body();
     int get_run(bool *run) { *run = m_Run; return YT_SUCCESS; };
     int set_run(bool run) { m_Run = run; return YT_SUCCESS; };
-    int get_status(int *status) { *status = m_Status; return YT_SUCCESS; };
-    int set_status(int status) { m_Status = status; return YT_SUCCESS; };
+    int get_status(short *status) { *status = m_Status; return YT_SUCCESS; };
+    int set_status(short status) { m_Status = status; return YT_SUCCESS; };
     int print_error();
 };
 #endif // #ifndef __FUNC_STATUS_H__
--- src/func_status.cpp
+++ src/func_status.cpp
@@ -1,7 +1,23 @@
 #include "func_status.h"
+#include <string.h>
 
+
+//-------------------------------------------------------------------------------------------------------
+// Class       :  func_status
+// Method      :  Constructor
+//
+// Notes       :  1. Initialize m_FuncName, m_Run, m_Status:
+//                   (1) m_FuncName: function name, does not include argument
+//                   (2)      m_Run: true  -> will run in next iteration (default)
+//                                   false -> will idle in next iteration
+//                   (3)   m_Status:     1 -> success
+//                                       0 -> failed
+//                                      -1 -> not execute by yt_inline/yt_inline_argument yet (default)
+//
+// Arguments   :  char    *func_name: inline function name
+//-------------------------------------------------------------------------------------------------------
 func_status::func_status(char *func_name)
-: m_Run(1), m_Status(0)
+: m_Run(true), m_Status(-1)
 {
     // copy func_name to m_FuncName
     int len = strlen(func_name);
