b28e32ceb7d37150873af0cfdc7fb09b6fcab783
cindytsai <6188366+cindytsai@users.noreply.github.com>
2022-12-25 21:14:40 -0600

    Make INTERACTIVE_MODE a default feature for now, probably add another header later.

--- include/libyt.h
+++ include/libyt.h
@@ -41,9 +41,7 @@ int yt_free_gridsPtr();
 int yt_inline_argument( char *function_name, int argc, ... );
 int yt_inline( char *function_name );
 
-#ifdef INTERACTIVE_MODE
 int yt_interactive_mode(char* flag_file_name);
-#endif
 
 // For derived field function to get grid information by GID and by field_name.
 int yt_getGridInfo_Dimensions( const long gid, int (*dimensions)[3] );
