08d7499a3f73cf4438509063462420bd2914e8af
cindytsai <6188366+cindytsai@users.noreply.github.com>
2023-01-25 10:55:23 -0600

    More comments in example.cpp yt_interactive_mode

--- example/example.cpp
+++ example/example.cpp
@@ -424,8 +424,8 @@ int main(int argc, char *argv[]) {
         // =======================================================================================================
         // libyt: 9. activate python prompt in interactive mode, should call it after yt_inline/yt_inline_argument
         // =======================================================================================================
-        // only supports when compile libyt using -DINTERACTIVE_MODE 
-        // (needs "libyt_interactive_mode.h" header)
+        // only supports when compile libyt using -DINTERACTIVE_MODE (needs "libyt_interactive_mode.h" header)
+        // when detecting "LIBYT_STOP" file, or any inline function failed, interactive prompt will start
         // if (yt_interactive_mode("LIBYT_STOP") != YT_SUCCESS) {
         //     fprintf(stderr, "ERROR: yt_interactive_mode failed!\n");
         //     exit(EXIT_FAILURE);
