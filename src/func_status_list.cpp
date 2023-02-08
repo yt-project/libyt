5bb630237239a06fcdfce61b13b58853fd6d9445
cindytsai <6188366+cindytsai@users.noreply.github.com>
2023-02-08 10:51:14 -0600

    Fix the bug: make func_status_list::run_func use PyRun_SimpleString.
    Make yt_run_Function* and this method running python function in the same way.

--- src/func_status_list.cpp
+++ src/func_status_list.cpp
@@ -144,8 +144,9 @@ int func_status_list::add_new_func(const char *func_name, int run) {
 //
 // Notes       :  1. This is a collective call. It executes new added functions that haven't run by
 //                   yt_run_Function/yt_run_FunctionArguments yet.
-//                2. Only supports function that don't have input arguments. It runs function func() in
-//                   Python.
+//                2. How this method runs python function is identical to yt_run_Function*. It use
+//                   PyRun_SimpleString.
+//                3. Get input arguments from func_status.m_Args if it has.
 //
 // Arguments   :  None
 //
@@ -156,28 +157,25 @@ int func_status_list::run_func() {
         int run = m_FuncStatusList[i].get_run();
         int status = m_FuncStatusList[i].get_status();
         if (run == 1 && status == -1) {
-            // TODO: Run this like how we run in yt_run_Function*, the error is caused by no traceback import.
             // command
-            char *funcname = m_FuncStatusList[i].get_func_name();
-            int command_width = 150 + strlen(g_param_libyt.script) + strlen(funcname) * 2;
+            const char *funcname = m_FuncStatusList[i].get_func_name();
+            int command_width = 300 + strlen(g_param_libyt.script) + strlen(funcname) * 2;
             char *command = (char*) malloc(command_width * sizeof(char));
             sprintf(command, "try:\n"
-                             "    %s(%s)\n"
+                             "    exec(\"%s(%s)\", sys.modules[\"%s\"].__dict__)\n"
                              "except Exception as e:\n"
                              "    libyt.interactive_mode[\"func_err_msg\"][\"%s\"] = traceback.format_exc()\n",
-                             funcname, m_FuncStatusList[i].get_args().c_str(), funcname);
+                             funcname, m_FuncStatusList[i].get_args().c_str(), g_param_libyt.script, funcname);
 
             // run and update status
+            log_info("Performing YT inline analysis %s(%s) ...\n", funcname, m_FuncStatusList[i].get_args().c_str());
             m_FuncStatusList[i].set_status(-2);
-
-            PyObject *global_var = PyDict_GetItemString(g_py_interactive_mode, "script_globals");
-            PyObject *py_src = Py_CompileString(command, "", Py_single_input);
-            PyObject *py_dum = PyEval_EvalCode(py_src, global_var, global_var);
-            Py_XDECREF(py_src);
-            Py_XDECREF(py_dum);
-            PyErr_Clear();
-            
+            if (PyRun_SimpleString(command) != 0) {
+                m_FuncStatusList[i].set_status(0);
+                YT_ABORT("Invoking \"%s(%s)\" ... failed\n", funcname, m_FuncStatusList[i].get_args().c_str());
+            }
             m_FuncStatusList[i].get_status();
+            log_info("Performing YT inline analysis %s(%s) ... done\n", funcname, m_FuncStatusList[i].get_args().c_str());
 
             // clean up
             free(command);
