b82380866d94e4aca05b6456c1fa76838373ca64
cindytsai <turquoisea.tsai@gmail.com>
2023-11-23 15:16:08 -0600

    Implement execute_request_impl for libyt kernel.
    Serial only, will expand.

---
+++ include/libyt_kernel.h
@@ -0,0 +1,27 @@
+#ifndef __LIBYT_KERNEL_H__
+#define __LIBYT_KERNEL_H__
+
+#include <Python.h>
+
+#include "nlohmann/json.hpp"
+#include "xeus/xinterpreter.hpp"
+
+class LibytKernel : public xeus::xinterpreter {
+public:
+    LibytKernel() = default;
+    virtual ~LibytKernel() = default;
+
+private:
+    PyObject* m_py_global;
+
+    void configure_impl() override;
+    nl::json execute_request_impl(int execution_counter, const std::string& code, bool silent, bool store_history,
+                                  nl::json user_expressions, bool allow_stdin) override;
+    nl::json complete_request_impl(const std::string& code, int cursor_pos) override;
+    nl::json inspect_request_impl(const std::string& code, int cursor_pos, int detail_level) override;
+    nl::json is_complete_request_impl(const std::string& code) override;
+    nl::json kernel_info_request_impl() override;
+    void shutdown_request_impl() override;
+};
+
+#endif  // __LIBYT_KERNEL_H__
