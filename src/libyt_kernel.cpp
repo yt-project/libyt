c2210fbf680809dc638d0b3e7ab98c4d7d34d753
cindytsai <turquoisea.tsai@gmail.com>
2023-11-28 19:45:42 -0600

    Single out execute_cell, prepare to support parallel.

--- src/libyt_kernel.cpp
+++ src/libyt_kernel.cpp
@@ -107,75 +107,17 @@ nl::json LibytKernel::execute_request_impl(int execution_counter, const std::str
     Py_DECREF(py_result);
     Py_DECREF(py_result_body);
 
-    // Clear the template buffer and redirect stdout, stderr
-    PyRun_SimpleString("sys.OUTPUT_STDOUT=''\nstdout_buf=io.StringIO()\nsys.stdout=stdout_buf\n");
-    PyRun_SimpleString("sys.OUTPUT_STDERR=''\nstderr_buf=io.StringIO()\nsys.stderr=stderr_buf\n");
-
-    // Execute upper half and lower half in serial, if error occurred, it will skip execute the lower half
-    PyObject *py_src, *py_dump;
-    bool has_error = false;
-    for (int i = 0; i < 2; i++) {
-        if (code_split[i].length() <= 0) continue;
-
-        // Compile code
-        if (i == 0) {
-            py_src = Py_CompileString(code_split[i].c_str(), cell_name.c_str(), Py_file_input);
-        } else if (i == 1) {
-            py_src = Py_CompileString(code_split[i].c_str(), cell_name.c_str(), Py_single_input);
-        }
-
-        // Evaluate code
-        if (py_src != NULL) {
-            py_dump = PyEval_EvalCode(py_src, LibytPythonShell::get_script_namespace(),
-                                      LibytPythonShell::get_script_namespace());
-            if (PyErr_Occurred()) {
-                has_error = true;
-                PyErr_Print();
-                Py_DECREF(py_src);
-                Py_XDECREF(py_dump);
-                break;
-            }
-            Py_DECREF(py_src);
-            Py_XDECREF(py_dump);
-        } else {
-            has_error = true;
-            PyErr_Print();
-            break;
-        }
-    }
-
-    // Collect stdout_buf/stderr_buf and store under sys.OUTPUT_STDOUT and sys.OUTPUT_STDERR
-    PyRun_SimpleString("sys.stdout.flush()\n");
-    PyRun_SimpleString("sys.stderr.flush()\n");
-    PyRun_SimpleString("sys.OUTPUT_STDOUT=stdout_buf.getvalue()\nstdout_buf.close()\n");
-    PyRun_SimpleString("sys.OUTPUT_STDERR=stderr_buf.getvalue()\nstderr_buf.close()\n");
-    PyRun_SimpleString("sys.stdout=sys.__stdout__\n");
-    PyRun_SimpleString("sys.stderr=sys.__stderr__\n");
-    PyErr_Clear();
-
-    // Parse the string
-    PyObject* py_module_sys = PyImport_ImportModule("sys");
-    PyObject* py_stdout_buf = PyObject_GetAttrString(py_module_sys, "OUTPUT_STDOUT");
-    PyObject* py_stderr_buf = PyObject_GetAttrString(py_module_sys, "OUTPUT_STDERR");
-
-    std::string output_stdout, output_stderr;
-    output_stdout = std::string(PyUnicode_AsUTF8(py_stdout_buf));
-    if (has_error) {
-        output_stderr = std::string(PyUnicode_AsUTF8(py_stderr_buf));
-    }
-
-    Py_DECREF(py_module_sys);
-    Py_DECREF(py_stdout_buf);
-    Py_DECREF(py_stderr_buf);
+    // Call execute cell
+    std::array<std::string, 2> output = LibytPythonShell::execute_cell(code_split, cell_name);
 
     // Publish results
     nl::json pub_data;
-    if (output_stdout.length() > 0) {
-        pub_data["text/plain"] = output_stdout.c_str();
+    if (output[0].length() > 0) {
+        pub_data["text/plain"] = output[0].c_str();
         publish_execution_result(execution_counter, std::move(pub_data), nl::json::object());
     }
-    if (has_error) {
-        publish_execution_error("", "", split(output_stderr, "\n"));
+    if (output[1].length() > 0) {
+        publish_execution_error("", "", split(output[1], "\n"));
     }
 
     return xeus::create_successful_reply();
