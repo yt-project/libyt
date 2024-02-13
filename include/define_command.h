f67d25f5f54751e00eb30996f1f9481936cf21d4
cindytsai <turquoisea.tsai@gmail.com>
2024-02-12 22:44:31 -0600

    The command should be made private, it's called by run.

--- include/define_command.h
+++ include/define_command.h
@@ -11,10 +11,6 @@ private:
     bool m_Undefine;
     static int s_Root;
 
-public:
-    define_command() : m_Command(""), m_Undefine(true){};
-    std::array<bool, 2> run(const std::string& command = std::string(""));
-
     int print_status();
     int print_help_msg();
     int load_script(const char* filename);
@@ -22,6 +18,10 @@ public:
     int set_func_run(const char* funcname, bool run);
     int set_func_run(const char* funcname, bool run, std::vector<std::string>& arg_list);
     int get_func_status(const char* funcname);
+
+public:
+    define_command() : m_Command(""), m_Undefine(true){};
+    std::array<bool, 2> run(const std::string& command = std::string(""));
 };
 
 #endif  //__DEFINE_COMMAND_H__
