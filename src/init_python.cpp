8f47c594b33331bd761b80d84b213028046277d5
cindytsai <turquoisea.tsai@gmail.com>
2024-09-19 12:17:24 -0500

    Using pybind11 to initialize Python.

--- src/init_python.cpp
+++ src/init_python.cpp
@@ -3,6 +3,11 @@
 #include "yt_combo.h"
 #undef CALL_IMPORT_ARRAY
 
+#ifdef USE_PYBIND11
+#include "pybind11/embed.h"
+#include "pybind11/numpy.h"
+#endif
+
 static int import_numpy();
 
 //-------------------------------------------------------------------------------------------------------
@@ -23,6 +28,7 @@ static int import_numpy();
 int init_python(int argc, char* argv[]) {
     SET_TIMER(__PRETTY_FUNCTION__);
 
+#ifndef USE_PYBIND11
     // 0: skips the initialization registration of signal handlers
     Py_InitializeEx(0);
     if (Py_IsInitialized())
@@ -40,6 +46,20 @@ int init_python(int argc, char* argv[]) {
         PyErr_PrintEx(0);
         YT_ABORT("Importing NumPy ... failed!\n");
     }
+#else
+    pybind11::initialize_interpreter();
+
+    // TODO: I don't know why I need to do this manually, it should be done in pybind11.
+    // import numpy
+    if (import_numpy())
+        log_debug("Importing NumPy ... done\n");
+    else {
+        // call _import_array and PyErr_PrintEx(0) to print out traceback error messages to stderr
+        _import_array();
+        PyErr_PrintEx(0);
+        YT_ABORT("Importing NumPy ... failed!\n");
+    }
+#endif  // #ifndef USE_PYBIND11
 
     // add the current location to the module search path
     if (PyRun_SimpleString("import sys; sys.path.insert(0, '.')") == 0) {
@@ -98,10 +118,8 @@ int init_python(int argc, char* argv[]) {
 int import_numpy() {
     SET_TIMER(__PRETTY_FUNCTION__);
 
-    // TODO: Cannot find import_array1, but compile success
     // import_array1() is a macro which calls _import_array() and returns the given value (YT_FAIL here) on error
     import_array1(YT_FAIL);
 
     return YT_SUCCESS;
-
 }  // FUNCTION : import_numpy
