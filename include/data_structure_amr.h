7b3c5970bd9faf7c27e0e5a9e01a85f976a29154
cindytsai <turquoisea.tsai@gmail.com>
2025-01-02 11:15:38 -0600

    [Refactor -- DS Amr] Make sure it is larger than 0.

--- include/data_structure_amr.h
+++ include/data_structure_amr.h
@@ -94,8 +94,8 @@ public:
 
     // TODO: return status
     DataStructureAmr();
-    void SetUp(long num_grids, int num_grids_local, int num_fields, int num_par_types, yt_par_type* par_type_list,
-               int index_offset);
+    DataStructureOutput SetUp(long num_grids, int num_grids_local, int num_fields, int num_par_types,
+                              yt_par_type* par_type_list, int index_offset);
     void SetPythonBindings(PyObject* py_hierarchy, PyObject* py_grid_data, PyObject* py_particle_data);
     void BindAllHierarchyToPython(int mpi_root, bool check_data);
     void BindLocalDataToPython();
