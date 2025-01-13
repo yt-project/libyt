814aca52675b0ecb2f4cf1728b8ed4b5c9109932
cindytsai <turquoisea.tsai@gmail.com>
2025-01-13 02:41:57 -0600

    [Refactor -- DS Amr] remove redundant stuff.

--- include/yt_prototype.h
+++ include/yt_prototype.h
@@ -19,10 +19,7 @@
 int big_MPI_Get_dtype(void* recv_buff, long data_len, yt_dtype* data_dtype, MPI_Datatype* mpi_dtype, int get_rank,
                       MPI_Aint base_address, MPI_Win* window);
 int get_mpi_dtype(yt_dtype data_type, MPI_Datatype* mpi_dtype);
-int check_hierarchy(yt_hierarchy*& hierarchy);
 int check_sum_num_grids_local_MPI(int mpi_size, int* num_grids_local_MPI);
-#else
-int check_hierarchy(yt_grid*& hierarchy);
 #endif
 
 void log_info(const char* Format, ...);
