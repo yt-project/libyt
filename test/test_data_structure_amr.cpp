fbf71d3e7b343f8887d173f618c658c87e524d05
cindytsai <turquoisea.tsai@gmail.com>
2025-01-20 10:20:29 -0600

    [Unit Test] Generate answer in assert section.

--- test/test_data_structure_amr.cpp
+++ test/test_data_structure_amr.cpp
@@ -60,29 +60,49 @@ protected:
     PyObject* GetPyHierarchy() const { return py_hierarchy_; }
     PyObject* GetPyGridData() const { return py_grid_data_; }
     PyObject* GetPyParticleData() const { return py_particle_data_; }
-    void GenerateLocalHierarchy(long num_grids, int index_offset, yt_grid* grids_local, int num_grids_local) {
+    void GenerateLocalHierarchy(long num_grids, int index_offset, yt_grid* grids_local, int num_grids_local,
+                                int num_par_types) {
         // Calculate range based on mpi rank
         long start_i = GetMpiRank() * (num_grids / GetMpiSize());
 
-        // Domain dimensions
+        // Generate local hierarchy
+        for (int i = 0; i < num_grids_local; i++) {
+            long gid = start_i + i + index_offset;
+            grids_local[i].id = gid;
+            GetGridHierarchy(gid, index_offset, &grids_local[i].parent_id, &grids_local[i].level,
+                             grids_local[i].grid_dimensions, grids_local[i].left_edge, grids_local[i].right_edge,
+                             num_grids, num_par_types, grids_local[i].par_count_list, nullptr);
+        }
+    }
+    void GetGridHierarchy(long gid, int index_offset, long* parent_id_ptr, int* level_ptr, int* grid_dim_ptr,
+                          double* grid_left_edge_ptr, double* grid_right_edge_ptr, long num_grids, int num_par_types,
+                          long* par_count_list = nullptr, int* proc_num_ptr = nullptr) {
+        // Info for creating a grid hierarchy based on gid
         int grid_dim[3] = {10, 1, 1};
         double dx_grid = 1.0;
         double domain_left_edge[3] = {0.0, 0.0, 0.0};
         double domain_right_edge[3] = {dx_grid * (double)num_grids, dx_grid * (double)num_grids,
                                        dx_grid * (double)num_grids};
 
-        for (int i = 0; i < num_grids_local; i++) {
-            long gid = start_i + i + index_offset;
-            grids_local[i].id = gid;
-            grids_local[i].parent_id = -1;
-            grids_local[i].level = 0;
-            for (int d = 0; d < 3; d++) {
-                grids_local[i].grid_dimensions[d] = grid_dim[d];
-                grids_local[i].left_edge[d] = domain_left_edge[d] + dx_grid * (double)(start_i + i);
-                grids_local[i].right_edge[d] = domain_left_edge[d] + dx_grid * (double)(start_i + i + 1);
+        // Generate and assign to input parameters
+        *parent_id_ptr = -1;
+        *level_ptr = 0;
+        for (int d = 0; d < 3; d++) {
+            grid_dim_ptr[d] = grid_dim[d];
+            grid_left_edge_ptr[d] = domain_left_edge[d] + dx_grid * ((double)gid - index_offset);
+            grid_right_edge_ptr[d] = domain_left_edge[d] + dx_grid * ((double)gid - index_offset + 1.0);
+        }
+
+        if (par_count_list != nullptr) {
+            for (int p = 0; p < num_par_types; p++) {
+                par_count_list[p] = gid;
             }
-            for (int p = 0; p < 2; p++) {
-                grids_local[i].par_count_list[p] = gid;
+        }
+
+        if (proc_num_ptr != nullptr) {
+            *proc_num_ptr = ((int)gid - index_offset) / (num_grids / GetMpiSize());
+            if (*proc_num_ptr == GetMpiSize()) {
+                *proc_num_ptr = GetMpiSize() - 1;
             }
         }
     }
@@ -114,7 +134,7 @@ TEST_P(TestDataStructureAmrHierarchy, Can_gather_local_hierarchy_and_bind_all_hi
     std::cout << "(mpi_root, index_offset, num_grids, num_grids_local, check_data) = (" << mpi_root << ", "
               << index_offset << ", " << num_grids << ", " << num_grids_local << ", " << check_data << ")" << std::endl;
     ds_amr.AllocateStorage(num_grids, num_grids_local, 0, num_par_types, par_type_list, index_offset, check_data);
-    GenerateLocalHierarchy(num_grids, index_offset, ds_amr.GetGridsLocal(), num_grids_local);
+    GenerateLocalHierarchy(num_grids, index_offset, ds_amr.GetGridsLocal(), num_grids_local, num_par_types);
 
     // Act
     DataStructureOutput status = ds_amr.BindAllHierarchyToPython(mpi_root);
@@ -122,51 +142,56 @@ TEST_P(TestDataStructureAmrHierarchy, Can_gather_local_hierarchy_and_bind_all_hi
     // Assert it gets full hierarchy
     EXPECT_EQ(status.status, DataStructureStatus::kDataStructureSuccess) << status.error;
     for (int gid = index_offset; gid < num_grids + index_offset; gid++) {
+        // Get grid hierarchy
+        int ans_grid_dims[3];
+        double ans_grid_left_edge[3], ans_grid_right_edge[3];
+        long ans_parent_id;
+        int ans_level, ans_proc_num;
+        long ans_par_count[2];
+        GetGridHierarchy(gid, index_offset, &ans_parent_id, &ans_level, ans_grid_dims, ans_grid_left_edge,
+                         ans_grid_right_edge, num_grids, num_par_types, ans_par_count, &ans_proc_num);
+
+        //
         int grid_dims[3];
         status = ds_amr.GetPythonBoundFullHierarchyGridDimensions(gid, grid_dims);
         EXPECT_EQ(status.status, DataStructureStatus::kDataStructureSuccess) << status.error;
-        EXPECT_EQ(grid_dims[0], 10);
-        EXPECT_EQ(grid_dims[1], 1);
-        EXPECT_EQ(grid_dims[2], 1);
+        for (int d = 0; d < 3; d++) {
+            EXPECT_EQ(grid_dims[d], ans_grid_dims[d]);
+        }
 
         double grid_left_edge[3], grid_right_edge[3];
         status = ds_amr.GetPythonBoundFullHierarchyGridLeftEdge(gid, grid_left_edge);
         EXPECT_EQ(status.status, DataStructureStatus::kDataStructureSuccess) << status.error;
         for (int d = 0; d < 3; d++) {
-            EXPECT_EQ(grid_left_edge[d], (double)gid - index_offset);
+            EXPECT_EQ(grid_left_edge[d], ans_grid_left_edge[d]);
         }
         status = ds_amr.GetPythonBoundFullHierarchyGridRightEdge(gid, grid_right_edge);
         EXPECT_EQ(status.status, DataStructureStatus::kDataStructureSuccess) << status.error;
         for (int d = 0; d < 3; d++) {
-            EXPECT_EQ(grid_right_edge[d], (double)gid - index_offset + 1.0);
+            EXPECT_EQ(grid_right_edge[d], ans_grid_right_edge[d]);
         }
 
         long parent_id = -2;
         status = ds_amr.GetPythonBoundFullHierarchyGridParentId(gid, &parent_id);
         EXPECT_EQ(status.status, DataStructureStatus::kDataStructureSuccess) << status.error;
-        EXPECT_EQ(parent_id, -1);
+        EXPECT_EQ(parent_id, ans_parent_id);
 
         int level = -2;
         status = ds_amr.GetPythonBoundFullHierarchyGridLevel(gid, &level);
         EXPECT_EQ(status.status, DataStructureStatus::kDataStructureSuccess) << status.error;
-        EXPECT_EQ(level, 0);
+        EXPECT_EQ(level, ans_level);
 
         int proc_num = -2;
         status = ds_amr.GetPythonBoundFullHierarchyGridProcNum(gid, &proc_num);
         EXPECT_EQ(status.status, DataStructureStatus::kDataStructureSuccess) << status.error;
-        int ans_proc_num = ((int)gid - index_offset) / (num_grids / GetMpiSize());
-        if (ans_proc_num == GetMpiSize()) {
-            ans_proc_num = GetMpiSize() - 1;
-        }
         EXPECT_EQ(proc_num, ans_proc_num);
 
         long par_count = -2;
-        status = ds_amr.GetPythonBoundFullHierarchyGridParticleCount(gid, "dark_matter", &par_count);
-        EXPECT_EQ(status.status, DataStructureStatus::kDataStructureSuccess) << status.error;
-        EXPECT_EQ(par_count, gid);
-        status = ds_amr.GetPythonBoundFullHierarchyGridParticleCount(gid, "star", &par_count);
-        EXPECT_EQ(status.status, DataStructureStatus::kDataStructureSuccess) << status.error;
-        EXPECT_EQ(par_count, gid);
+        for (int p = 0; p < num_par_types; p++) {
+            status = ds_amr.GetPythonBoundFullHierarchyGridParticleCount(gid, par_type_list[p].par_type, &par_count);
+            EXPECT_EQ(status.status, DataStructureStatus::kDataStructureSuccess) << status.error;
+            EXPECT_EQ(par_count, ans_par_count[p]);
+        }
     }
 
     // Clean up
