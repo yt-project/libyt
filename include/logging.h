cc88156cdeebf8a7d2151acaf9b6f654b3d3bf27
cindytsai <turquoisea.tsai@gmail.com>
2025-01-31 11:58:34 -0600

    move YT_ABORT to logging.h

--- include/logging.h
+++ include/logging.h
@@ -8,4 +8,11 @@ void LogDebug(const char* format, ...);
 void LogError(const char* format, ...);
 }  // namespace logging
 
+#define YT_ABORT(...)                                                                                                  \
+    {                                                                                                                  \
+        logging::LogError(__VA_ARGS__);                                                                                \
+        fprintf(stderr, "%13s==> file <%s>, line <%d>, function <%s>\n", "", __FILE__, __LINE__, __FUNCTION__);        \
+        return YT_FAIL;                                                                                                \
+    }
+
 #endif  // LIBYT_PROJECT_INCLUDE_LOGGING_H_
