0ced4b2a3c3f2a9287519b88d5c0855245c1b2bf
cindytsai <turquoisea.tsai@gmail.com>
2025-01-31 16:08:09 -0600

    move check and print yt_param_yt into yt_set_Parameters.
    Probably should move to individual files if later I have make a class clearly defining what should be in a custom type. For now, just stay there.

--- src/yt_set_Parameters.cpp
+++ src/yt_set_Parameters.cpp
@@ -3,11 +3,13 @@
 #include "logging.h"
 #include "python_controller.h"
 #include "timer.h"
-#include "yt_prototype.h"
 
 #ifdef USE_PYBIND11
 #include "pybind11/embed.h"
 #endif
+
+static int CheckYtParamYt(const yt_param_yt& param_yt);
+static int PrintYtParamYt(const yt_param_yt& param_yt);
 
 //-------------------------------------------------------------------------------------------------------
 // Function    :  yt_set_Parameters
@@ -54,14 +56,15 @@ int yt_set_Parameters(yt_param_yt* input_param_yt) {
     }
 
     // check if all parameters have been set properly
-    if (check_yt_param_yt(*input_param_yt))
+    if (CheckYtParamYt(*input_param_yt)) {
         logging::LogDebug("Validating YT parameters ... done\n");
-    else
+    } else {
         YT_ABORT("Validating YT parameters ... failed\n");
+    }
 
     // print out all parameters
     logging::LogDebug("List of YT parameters:\n");
-    print_yt_param_yt(*input_param_yt);
+    PrintYtParamYt(*input_param_yt);
 
     // store user-provided parameters to a libyt internal variable
     LibytProcessControl::Get().param_yt_ = *input_param_yt;
@@ -188,3 +191,129 @@ int yt_set_Parameters(yt_param_yt* input_param_yt) {
     return YT_SUCCESS;
 
 }  // FUNCTION : yt_set_Parameters
+
+//-------------------------------------------------------------------------------------------------------
+// Function    :  CheckYtParamYt
+// Description :  Check yt_param_yt struct
+//
+// Note        :  1. Cosmological parameters are checked only if cosmological_simulation == 1
+//                2. Only check if data are set properly, does not alter them.
+//
+// Parameter   :  const yt_param_yt &param_yt
+//
+// Return      :  YT_SUCCESS or YT_FAIL
+//-------------------------------------------------------------------------------------------------------
+static int CheckYtParamYt(const yt_param_yt& param_yt) {
+    SET_TIMER(__PRETTY_FUNCTION__);
+
+    if (param_yt.frontend == NULL) YT_ABORT("\"%s\" has not been set!\n", "frontend");
+    for (int d = 0; d < 3; d++) {
+        if (param_yt.domain_left_edge[d] == DBL_UNDEFINED)
+            YT_ABORT("\"%s[%d]\" has not been set!\n", "domain_left_edge", d);
+        if (param_yt.domain_right_edge[d] == DBL_UNDEFINED)
+            YT_ABORT("\"%s[%d]\" has not been set!\n", "domain_right_edge", d);
+    }
+    if (param_yt.current_time == DBL_UNDEFINED) YT_ABORT("\"%s\" has not been set!\n", "current_time");
+    if (param_yt.cosmological_simulation == INT_UNDEFINED)
+        YT_ABORT("\"%s\" has not been set!\n", "cosmological_simulation");
+    if (param_yt.cosmological_simulation == 1) {
+        if (param_yt.current_redshift == DBL_UNDEFINED) YT_ABORT("\"%s\" has not been set!\n", "current_redshift");
+        if (param_yt.omega_lambda == DBL_UNDEFINED) YT_ABORT("\"%s\" has not been set!\n", "omega_lambda");
+        if (param_yt.omega_matter == DBL_UNDEFINED) YT_ABORT("\"%s\" has not been set!\n", "omega_matter");
+        if (param_yt.hubble_constant == DBL_UNDEFINED) YT_ABORT("\"%s\" has not been set!\n", "hubble_constant");
+    }
+    if (param_yt.length_unit == DBL_UNDEFINED) YT_ABORT("\"%s\" has not been set!\n", "length_unit");
+    if (param_yt.mass_unit == DBL_UNDEFINED) YT_ABORT("\"%s\" has not been set!\n", "mass_unit");
+    if (param_yt.time_unit == DBL_UNDEFINED) YT_ABORT("\"%s\" has not been set!\n", "time_unit");
+    if (param_yt.velocity_unit == DBL_UNDEFINED) YT_ABORT("\"%s\" has not been set!\n", "velocity_unit");
+    if (param_yt.magnetic_unit == DBL_UNDEFINED) logging::LogWarning("\"%s\" has not been set!\n", "magnetic_unit");
+
+    for (int d = 0; d < 3; d++) {
+        if (param_yt.periodicity[d] == INT_UNDEFINED) YT_ABORT("\"%s[%d]\" has not been set!\n", "periodicity", d);
+        if (param_yt.domain_dimensions[d] == INT_UNDEFINED)
+            YT_ABORT("\"%s[%d]\" has not been set!\n", "domain_dimensions", d);
+    }
+    if (param_yt.dimensionality == INT_UNDEFINED) YT_ABORT("\"%s\" has not been set!\n", "dimensionality");
+    if (param_yt.refine_by == INT_UNDEFINED) YT_ABORT("\"%s\" has not been set!\n", "refine_by");
+    if (param_yt.num_grids == LNG_UNDEFINED) YT_ABORT("\"%s\" has not been set!\n", "num_grids");
+    if (param_yt.num_par_types > 0 && param_yt.par_type_list == NULL)
+        YT_ABORT("Particle type info par_type_list has not been set!\n");
+    if (param_yt.num_par_types < 0 && param_yt.par_type_list != NULL)
+        YT_ABORT("Particle type info num_par_types has not been set!\n");
+    for (int s = 0; s < param_yt.num_par_types; s++) {
+        if (param_yt.par_type_list[s].par_type == NULL || param_yt.par_type_list[s].num_attr < 0)
+            YT_ABORT("par_type_list element [ %d ] is not set properly!\n", s);
+    }
+
+    return YT_SUCCESS;
+}
+
+//-------------------------------------------------------------------------------------------------------
+// Function    :  PrintYtParamYt
+// Description :  Print yt_param_yt struct if verbose level >= YT_VERBOSE_DEBUG
+//
+// Parameter   :  const yt_param_yt &param_yt
+//
+// Return      :  YT_SUCCESS or YT_FAIL
+//-------------------------------------------------------------------------------------------------------
+static int PrintYtParamYt(const yt_param_yt& param_yt) {
+    SET_TIMER(__PRETTY_FUNCTION__);
+
+    if (LibytProcessControl::Get().param_libyt_.verbose < YT_VERBOSE_DEBUG) return YT_SUCCESS;
+
+    const int width_scalar = 25;
+    const int width_vector = width_scalar - 3;
+
+    if (param_yt.frontend != nullptr) logging::LogDebug("   %-*s = %s\n", width_scalar, "frontend", param_yt.frontend);
+    if (param_yt.fig_basename != nullptr)
+        logging::LogDebug("   %-*s = %s\n", width_scalar, "fig_basename", param_yt.fig_basename);
+    for (int d = 0; d < 3; d++) {
+        logging::LogDebug("   %-*s[%d] = %13.7e\n", width_vector, "domain_left_edge", d, param_yt.domain_left_edge[d]);
+    }
+    for (int d = 0; d < 3; d++) {
+        logging::LogDebug("   %-*s[%d] = %13.7e\n", width_vector, "domain_right_edge", d,
+                          param_yt.domain_right_edge[d]);
+    }
+    logging::LogDebug("   %-*s = %13.7e\n", width_scalar, "current_time", param_yt.current_time);
+    logging::LogDebug("   %-*s = %d\n", width_scalar, "cosmological_simulation", param_yt.cosmological_simulation);
+    if (param_yt.cosmological_simulation) {
+        logging::LogDebug("   %-*s = %13.7e\n", width_scalar, "current_redshift", param_yt.current_redshift);
+        logging::LogDebug("   %-*s = %13.7e\n", width_scalar, "omega_lambda", param_yt.omega_lambda);
+        logging::LogDebug("   %-*s = %13.7e\n", width_scalar, "omega_matter", param_yt.omega_matter);
+        logging::LogDebug("   %-*s = %13.7e\n", width_scalar, "hubble_constant", param_yt.hubble_constant);
+    }
+
+    logging::LogDebug("   %-*s = %13.7e\n", width_scalar, "length_unit", param_yt.length_unit);
+    logging::LogDebug("   %-*s = %13.7e\n", width_scalar, "mass_unit", param_yt.mass_unit);
+    logging::LogDebug("   %-*s = %13.7e\n", width_scalar, "time_unit", param_yt.time_unit);
+    logging::LogDebug("   %-*s = %13.7e\n", width_scalar, "velocity_unit", param_yt.velocity_unit);
+    if (param_yt.magnetic_unit == DBL_UNDEFINED)
+        logging::LogDebug("   %-*s = %s\n", width_scalar, "magnetic_unit", "NOT SET, and will be set to 1.");
+    else
+        logging::LogDebug("   %-*s = %13.7e\n", width_scalar, "magnetic_unit", param_yt.magnetic_unit);
+
+    for (int d = 0; d < 3; d++) {
+        logging::LogDebug("   %-*s[%d] = %d\n", width_vector, "periodicity", d, param_yt.periodicity[d]);
+    }
+    for (int d = 0; d < 3; d++) {
+        logging::LogDebug("   %-*s[%d] = %d\n", width_vector, "domain_dimensions", d, param_yt.domain_dimensions[d]);
+    }
+    logging::LogDebug("   %-*s = %d\n", width_scalar, "dimensionality", param_yt.dimensionality);
+    logging::LogDebug("   %-*s = %d\n", width_scalar, "refine_by", param_yt.refine_by);
+    logging::LogDebug("   %-*s = %d\n", width_scalar, "index_offset", param_yt.index_offset);
+    logging::LogDebug("   %-*s = %ld\n", width_scalar, "num_grids", param_yt.num_grids);
+
+    logging::LogDebug("   %-*s = %ld\n", width_scalar, "num_fields", param_yt.num_fields);
+    logging::LogDebug("   %-*s = %ld\n", width_scalar, "num_par_types", param_yt.num_par_types);
+    for (int s = 0; s < param_yt.num_par_types; s++) {
+        if (param_yt.par_type_list != nullptr)
+            logging::LogDebug("   %-*s[%d] = (type=\"%s\", num_attr=%d)\n", width_vector, "par_type_list", s,
+                              param_yt.par_type_list[s].par_type, param_yt.par_type_list[s].num_attr);
+        else
+            logging::LogDebug("   %-*s[%d] = (type=\"%s\", num_attr=%d)\n", width_vector, "par_type_list", s, "NULL",
+                              param_yt.par_type_list[s].num_attr);
+    }
+    logging::LogDebug("   %-*s = %ld\n", width_scalar, "num_grids_local", param_yt.num_grids_local);
+
+    return YT_SUCCESS;
+}
