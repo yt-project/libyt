fe03aacaab8e94b25cb18209c2480dbf338e4905
cindytsai <turquoisea.tsai@gmail.com>
2025-02-14 12:55:32 -0600

    [MemProf] Upload dummy artifacts.

--- test/memory_profile/memory_profile.cpp
+++ test/memory_profile/memory_profile.cpp
@@ -2,14 +2,34 @@
 
 #include <iostream>
 
-int main() {
+#ifndef SERIAL_MODE
+#include <mpi.h>
+#endif
+
+int main(int argc, char* argv[]) {
+    int my_rank;
+    int my_size;
+#ifndef SERIAL_MODE
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &my_size);
+#else
+    my_rank = 0;
+    my_size = 1;
+#endif
+
     const int kLenValgrind = 100;
     char valgrind[kLenValgrind];
+    const char* suffix = ".mem_prof";
 
     int* ptr_leak = new int[100];
 
-    snprintf(valgrind, kLenValgrind, "detailed_snapshot MPI%dAfterFree%d\0", 0, 0);
+    snprintf(valgrind, kLenValgrind, "detailed_snapshot MPI%dAfterFree%d%s\0", 0, 0, suffix);
     VALGRIND_MONITOR_COMMAND(valgrind);
+
+#ifndef SERIAL_MODE
+    MPI_Finalize();
+#endif
 
     return 0;
 }
