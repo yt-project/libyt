40506c55a12b369943ab932b48db307829b5f373
cindytsai <turquoisea.tsai@gmail.com>
2025-02-15 14:55:47 -0600

    [MemProf] Memory profile rma operation on mpi and derived on serial.

--- test/memory_profile/test_memory_profile.py
+++ test/memory_profile/test_memory_profile.py
@@ -2,13 +2,14 @@ import libyt
 
 try:
     from mpi4py import MPI
+
     comm = MPI.COMM_WORLD
     my_rank = comm.Get_rank()
     my_size = comm.Get_size()
-
 except ImportError:
     my_rank = 0
     my_size = 1
+
 
 def test_derived_function():
     start_id = my_rank * int(libyt.param_yt["num_grids"] / my_size)
@@ -20,3 +21,19 @@ def test_derived_function():
 
     for i in query_data:
         data = libyt.derived_func(i, "DerivedOnes")
+
+
+def test_rma_function():
+    start_id_list = [r * int(libyt.param_yt["num_grids"] / my_size) for r in range(my_size)]
+
+    data_len = 3000
+    my_data = list(range(start_id_list[my_rank], start_id_list[my_rank] + data_len))
+    query_data = []
+    data_location = []
+    for r in range(my_size):
+        if r != my_rank:
+            query_data += list(range(start_id_list[r], start_id_list[r] + data_len))
+            data_location += [r] * data_len
+
+    data = libyt.get_field_remote(["CCTwos"], len(my_data), my_data, len(query_data), query_data, data_location,
+                                  len(data_location))
