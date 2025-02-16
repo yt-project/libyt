9f79b77d761bb37ad5eb0dbfa8c612ffd6bd98f2
cindytsai <turquoisea.tsai@gmail.com>
2025-02-15 23:51:38 -0600

    [MemProf] try to fix the invalid output.

--- .github/tools/conclude_memory_profile.py
+++ .github/tools/conclude_memory_profile.py
@@ -1,3 +1,33 @@
+import os
+import numpy as np
+
+def extract_value_from_file(filename: str, attribute: str) -> np.array:
+    """extract_value_from_file
+    Assume attribute in a file is in a format 'attribute=value'.
+    """
+
+    # make sure file exist and read the file
+    if not os.path.exists(filename):
+        raise "No such file"
+    with open(filename, "r") as f:
+        raw = f.read()
+
+    # find all the matching attribute
+    found = -1
+    extract_value = []
+    while True:
+        # find matching attributes
+        found = raw.find(attribute, found + 1)
+        if found < 0:
+            break
+
+        found_newline = raw.find("\n", found)
+        extract_value.append(int(raw[found + len(attribute) + 1: found_newline]))
+
+    return np.array(extract_value)
+
 if __name__ == "__main__":
-    print(":baby_chick: Test comment 123 ...")
-    print(":baby_chick: Test comment 123 ...")
+
+    values = extract_value_from_file("AfterFree_rank0.mem_prof", "mem_heap_B")
+
+    print(values, "<br>", values)
